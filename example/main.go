4d9ace67b96a4699aac68340a8a71a7eaee469ba
Alex Bykov <alex@bykov.me>
2020-04-13 13:12:09 +0300

    fixing example

--- example/main.go
+++ example/main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/bykovme/gotrans"
 )
 
+const cHtmlTemplate = `
+<html>
+	<head>
+		<title>%s</title>
+	</head>
+	<body>
+		<h2> %s </h2>
+		%s 
+	</body>
+</html>
+`
+const cGitHubLink = "https://github.com/bykovme/gotrans"
+const cLink = `<a href="%s">%s</a>`
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	lang := gotrans.DetectLanguage(r.Header.Get("Accept-Language"))
-	fmt.Fprintf(w, "<html><head><title> %s </title></head><body>", gotrans.Tr(lang, "hello_world"))
-	fmt.Fprintf(w, "<h2> %s </h2>", gotrans.Tr(lang, "hello_world"))
-	githubLink := "https://github.com/bykovme/gotrans"
-	link := fmt.Sprintf(`<a href="%s">%s</a>`, githubLink, githubLink)
-	fmt.Fprintf(w, gotrans.Tr(lang, "find_more"), link)
-	fmt.Fprint(w, "</body></html>")
+	helloWorld := gotrans.Tr(lang, "hello_world")
+	link := fmt.Sprintf(cLink, cGitHubLink, cGitHubLink)
+	findMore := fmt.Sprintf(gotrans.Tr(lang, "find_more"), link)
+	_, err := fmt.Fprintf(w, cHtmlTemplate,
+		helloWorld,
+		helloWorld,
+		findMore)
+	if err != nil {
+		log.Println(err.Error())
+	}
 }
 
 func main() {
@@ -24,5 +43,5 @@ func main() {
 	}
 
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":3000", nil)
+	_ = http.ListenAndServe(":3000", nil)
 }
